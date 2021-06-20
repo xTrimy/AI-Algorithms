b9570649bb4a1816c771b6e05d3aa84f06d11333
Abdelrahman Sayed <61325578+xBodda@users.noreply.github.com>
2021-06-20 03:30:29 +0200

    Edit DT

--- Kmeans.py
+++ Kmeans.py
@@ -1,42 +1,50 @@
-
-import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
 import numpy as np
-from sklearn.datasets.samples_generator import make_blobs
-from sklearn.datasets import make_moons
-from sklearn.cluster import SpectralClustering
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('BrainTumor.csv')
+X = dataset.iloc[:, [0,14]].values
+# y = dataset.iloc[:, 3].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+# Using the elbow method to find the optimal number of clusters
 from sklearn.cluster import KMeans
-
-
-X, y_true = make_blobs(n_samples=500, centers=3, cluster_std= 0.6, random_state=40)
-plt.scatter(X[:, 0], X[:, 1], s=50)
-
-kmeans = KMeans(n_clusters=5)
-kmeans.fit(X)
-y_kmeans = kmeans.predict(X)
-plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
-centers = kmeans.cluster_centers_
-plt.scatter(centers[:, 0], centers[:, 1], c='black', s=50, alpha=1)
-
-x, y = make_moons(500, noise=.05, random_state=42)
-labels = KMeans(5, random_state=42).fit_predict(x)
-plt.scatter(x[:, 0], x[:, 1], c=labels, s=50, cmap='viridis')
-
-model = SpectralClustering(n_clusters=4, affinity='nearest_neighbors', assign_labels='kmeans')
-labels = model.fit_predict(x)
-plt.scatter(x[:, 0], x[:, 1], c=labels, s=50, cmap='viridis')
-
-intertia = []
-K = range(1,20)
-for k in K:
-    km = KMeans(n_clusters=k)
-    km = km.fit(X)
-    intertia.append(km.inertia_)
-
-plt.plot(K, intertia, marker= "x")
-plt.xlabel('k')
-plt.xticks(np.arange(20))
-plt.ylabel('Intertia')
-plt.title('Elbow Method')
+wcss = []
+for i in range(1, 2):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 2), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
 plt.show()
 
+# Fitting K-Means to the dataset
+kmeans = KMeans(n_clusters = 2, init = 'k-means++', random_state = 42)
+y_kmeans = kmeans.fit_predict(X)
+
+# Visualising the clusters
+plt.scatter(X[y_kmeans == 0, 1], X[y_kmeans == 0, 1], s = 10, c = 'blue', label = 'Cluster 1')
+plt.scatter(X[y_kmeans == 1, 1], X[y_kmeans == 1, 1], s = 10, c = 'red', label = 'Cluster 2')
+# plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 10, c = 'green', label = 'Cluster 3')
+# plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 10, c = 'cyan', label = 'Cluster 4')
+# plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 10, c = 'magenta', label = 'Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 100, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
